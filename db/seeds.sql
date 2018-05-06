ff8cce313e394a928084fc73d37307b8c07dfb95
Kevin Murtaugh <35198999+Kevin-Murtaugh@users.noreply.github.com>
2018-05-05 20:18:59 -0400

    updating logic

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (name) VALUES 
+('Wimpy burger (with olives)', "false"),
+('Popeye burger (with spinach)', "false"),
+('OliveOyl burger (tofu)', "false"),
+('Bluto burger (double meat, extra greasy)', "false")
+
+
+
